e2c8a670ed633a350122b22900cdc90acb7abe08
juancruz <juancruzmart93@gmail.com>
2024-03-15 01:15:15 -0300

    Actualización de Precios v1

--- src/app/aumento/aumento.component.ts
+++ src/app/aumento/aumento.component.ts
@@ -12,4 +12,27 @@ export class AumentoComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  onInputChange(): void {
+    console.log('INPUT CHANGE')
+  }
+
+  // Método para abrir el modal de actualización de precios
+  abrirModal() {
+    // Aquí puedes escribir la lógica necesaria para abrir el modal
+    // Por ejemplo, si estás usando Bootstrap, puedes usar jQuery o JavaScript nativo para activar el modal
+    // Si estás utilizando algún otro framework o librería de UI, el enfoque puede variar
+    // Por favor, asegúrate de ajustar este código según cómo hayas implementado tu modal
+    // Un ejemplo genérico sería:
+    const modal = document.getElementById('actualizarPreciosModal');
+    if (modal) {
+      modal.classList.add('show'); // Muestra el modal
+      modal.style.display = 'block'; // Asegura que el modal esté visible
+      // También puedes necesitar activar otros eventos o configuraciones relacionadas con el modal aquí
+    }
+  }
+
+  actualizarPrecios({ value, valid }: { value: {}, valid: boolean }) {
+    console.log('ACTUALIZA PRECIOS');
+  }
+
 }
